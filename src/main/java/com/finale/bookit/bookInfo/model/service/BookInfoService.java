c7b67a3b490a734cc73b3bf82221f98701639e53
leehb105 <leesonge95@gmail.com>
2022-01-27 02:44:50 +0900

    클래스 추가

---
+++ src/main/java/com/finale/bookit/bookInfo/model/service/BookInfoService.java
@@ -0,0 +1,5 @@
+package com.finale.bookit.bookInfo.model.service;
+
+public interface BookInfoService {
+	public void selectBookInfo();
+}
---
+++ src/main/java/com/finale/bookit/bookInfo/model/service/BookInfoServiceImpl.java
@@ -0,0 +1,12 @@
+package com.finale.bookit.bookInfo.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookInfoServiceImpl implements BookInfoService {
+
+	@Override
+	public void selectBookInfo() {
+	}
+
+}
